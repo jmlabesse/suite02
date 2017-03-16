26654eeef25b8fe1307fe51cc0df49a79082e7ba
jmlabesse <jeanmarie.labesse@gmail.com>
2017-03-16 13:50:46 +0100

    Résolution ex01

--- ex01.js
+++ ex01.js
@@ -27,3 +27,48 @@
 */
 
 // write your code below this comment
+function addition(nombre1, nombre2) {
+    return nombre1 + nombre2;
+}
+function soustraction(nombre1, nombre2) {
+    return nombre1 - nombre2;
+}
+function multiplication(nombre1, nombre2) {
+    return nombre1 * nombre2;
+}
+function division(nombre1, nombre2) {
+    return (nombre2 === 0 ? 0 : nombre1 / nombre2);
+}
+function modulo(nombre1, nombre2) {
+    return nombre1 % nombre2;
+}
+function evalExpr(nombre1, nombre2, operateur) {
+    switch (operateur) {
+        case "+":
+            return addition(nombre1, nombre2);
+            break;
+        case "-":
+            return soustraction(nombre1, nombre2);
+            break;
+        case "*":
+            return multiplication(nombre1, nombre2);
+            break;
+        case "/":
+            return division(nombre1, nombre2);
+            break;
+        case "%":
+            return modulo(nombre1, nombre2);
+            break;
+        default:
+            return "Opération impossible";
+    }
+}
+
+// Tests
+console.log(evalExpr(4, 5, "*")); // 20
+console.log(evalExpr(3, 3, "+")); // 6
+console.log(evalExpr(2, 2, "%")); // 0
+console.log(evalExpr(3, 3, "-")); // 0
+console.log(evalExpr(10, 2, "/")); // 5
+
+console.log(evalExpr(10, 0, "/")); // 0
