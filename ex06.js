7e8835ec586b82a203fa7b15f3ca482131bf750e
jmlabesse <jeanmarie.labesse@gmail.com>
2017-03-16 15:19:46 +0100

    Résolution ex06

--- ex06.js
+++ ex06.js
@@ -23,4 +23,28 @@
 */
 
 // write your code below this comment
+function bubbleSort(array) {
+    var sortedArray = [];
 
+    while (array.length > 0) {
+        var ind = minValueIndex(array);
+        sortedArray.push(array[ind]);
+        array.splice(ind, 1);
+    }
+    return sortedArray;
+}
+
+function minValueIndex(array) {
+    var min = array[0];
+    var minIndex = 0;
+    for (index in array) {
+        if (array[index] < min) {
+            min = array[index];
+            minIndex = index;
+        }
+    }
+    return minIndex;
+}
+
+console.log(bubbleSort([8, 3, 0]));
+console.log(bubbleSort([8, 3, 0, 1, 7, 4, 6, 12, 0, 5, 5]));
